b18bdaf7278b889e46cf3852db536130ebdad030
harshkuhikar <114164931+harshkuhikar@users.noreply.github.com>
2023-08-17 14:25:51 +0530

    .

---
+++ REACT JS/user/src/component/forms/multipleinputs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Multipleinputs = () => {
+    return (
+        <div>
+            <h1>Hello Harsh</h1>
+        </div>
+    )
+}
+
+export default Multipleinputs
