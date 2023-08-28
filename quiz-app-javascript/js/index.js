f57fd682335a4fa9f89f24b9aa6fc92a08c42e46
harshkuhikar <114164931+harshkuhikar@users.noreply.github.com>
2023-08-28 14:08:33 +0530

    .

---
+++ quiz-app-javascript/js/index.js
@@ -0,0 +1,92 @@
+const questions = [
+    {
+        'que': "Which of the following is a markup language",
+        'a': 'HTML',
+        'b': 'CSS',
+        'c': 'javaScript',
+        'd': 'PHP',
+        'correct': 'a',
+    },
+    {
+        'que': "What year of javaScript launched",
+        'a': '1996',
+        'b': '1994',
+        'c': '1995',
+        'd': 'None Of This',
+        'correct': 'b',
+    },
+    {
+        'que': "What does CSS atnd for",
+        'a': 'Hypertext markup language',
+        'b': 'Cascading style sheet',
+        'c': 'jason object Notation',
+        'd': 'None Of This',
+        'correct': 'b'
+    }
+
+]
+
+let index = 0;
+let total = questions.length
+let right = 0,
+    wrong = 0;
+const quesBox = document.getElementById("quesBox")
+const optionInputs = document.querySelectorAll(".options")
+
+const loadQuestion = () => {
+    if(index === total){
+        return endQuiz()
+    }
+    reset()
+    const data = questions[index]
+    quesBox.innerText = `${index + 1} ${data.que}   `;
+    optionInputs[0].nextElementSibling.innerText = data.a;
+    optionInputs[1].nextElementSibling.innerText = data.b;
+    optionInputs[2].nextElementSibling.innerText = data.c;
+    optionInputs[3].nextElementSibling.innerText = data.d;
+}
+
+const submitQuiz = () => {
+    const data = questions[index];
+    const ans = getAnswer()
+    if (ans === data.correct) {
+        right++;
+    }
+    else {
+        wrong++;
+    }
+    index++;
+    loadQuestion();
+    return;
+}
+const getAnswer = () => {
+    let answer;
+    optionInputs.forEach(
+        (input) => {
+            if (input.checked) {
+                answer = input.value;
+                
+            }
+
+        }
+    )
+    return answer;
+}
+
+const reset = () => {
+    optionInputs.forEach(
+        (input) => {
+            input.checked = false;
+
+        }
+    )
+}
+
+const endQuiz = () =>{
+    document.getElementById("box").innerHTML = `
+    <div style="text-align: center;"><h3> Thank you for playing the Quiz</h3>
+    <h2>${right} / ${total} are correct</h2></div>
+    `
+}
+
+loadQuestion();
